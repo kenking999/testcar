f20eb71608ae86c71bc23dd4bffd867eeebd6f99
kenking999 <145443757+kenking999@users.noreply.github.com>
2025-01-28 15:57:46 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
 basic.forever(function () {
-	
+    IdeaKitMaker.MotorRun(IdeaKitMaker.Motors.M2A, 250)
+    IdeaKitMaker.MotorRun(IdeaKitMaker.Motors.M1A, 250)
 })
